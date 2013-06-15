de12a2b1c31a44e8e56faae5c6fee4214e7d6c62
Gary Rowe <g.rowe@froot.co.uk>
2013-06-15 21:58:22 +0100

    Initial commit supporting basic whitelist

--- README.md
+++ README.md
@@ -1,4 +1,71 @@
-BitcoinjEnforcerRules
-=====================
+Status: [![Build Status](https://travis-ci.org/gary-rowe/BitcoinEnforcerRules.png?branch=master)](https://travis-ci.org/gary-rowe/BitcoinEnforcerRules)
 
-Verifies Maven checksums against a known list to deter side-chain attack vectors
+## Bitcoinj Enforcer Rules
+
+[Bitcoinj](https://code.google.com/p/bitcoinj/) is the first open source library for Java that allows you to spend money
+without involving a third party.
+
+This means that if the Bitcoinj JAR was corrupted, or made use of a corrupted library, then there is a high probability
+that your Bitcoin private keys would be obtained and used to spend any and all funds that they controlled.
+
+For example, if, say, SLF4J (populate logging framework) was hacked and code placed within to reflectively search for
+private keys how would you detect that in your Maven build? You may think that the SHA1 and MD5 signatures would protect
+you, but in the event of a successful attack against Maven Central they would match the digest of the downloaded
+artifact. In the absence of a controlled white list of permitted libraries there is little that can be done within the
+Maven environment to protect yourself against this kind of side-chain attack vector.
+
+The Bitcoin Enforcer Rules work with the [Maven Enforcer Plugin](http://maven.apache.org/enforcer/maven-enforcer-plugin/)
+to provide such a whitelist. You can choose how detailed you want it to be depending on your own security requirements,
+but bear in mind that every unchecked dependency is a way in for an attacker through the back door.
+
+## Example only! Do not use this configuration in production!
+
+This is not safe for production use. Make sure that you obtain the official version over HTTPS or through the Bitcoinj
+git repository when including it into your project.
+
+```xml
+<build>
+  <plugins>
+    ...
+    <plugin>
+      <groupId>org.apache.maven.plugins</groupId>
+      <artifactId>maven-enforcer-plugin</artifactId>
+      <version>1.2</version>
+      <executions>
+        <execution>
+          <id>enforce</id>
+          <configuration>
+            <rules>
+              <digestRule implementation="com.google.bitcoinj.enforcer.SHA1SignatureRule">
+                <!-- List of required hashes -->
+                <!-- Format is URN of groupId:artifactId:version:type:classifier:scope:algorithm:hash -->
+                <!-- classifier is "null" if not present -->
+                <!-- algorithm is "sha1" or "md5" with "sha1" preferred -->
+                <urns>
+                  <urn>com.google:bitcoinj:0.5.0:jar:null:compile:sha1:923164f40d38caa012ca08861092dd1d5ee6f4b9</urn>
+                  <urn>org.bouncycastle:bcprov-jdk15:1.46:jar:null:compile:md5:d726ceb2dcc711ef066cc639c12d856128ea1ef1</urn>
+                </urns>
+              </digestRule>
+            </rules>
+          </configuration>
+          <goals>
+            <goal>enforce</goal>
+          </goals>
+        </execution>
+      </executions>
+
+      <!-- Ensure the rules are downloaded (we can include them in the URN whitelist) -->
+      <dependencies>
+        <dependency>
+          <groupId>com.google.bitcoinj</groupId>
+          <artifactId>bitcoinj-com.google.bitcoinj.enforcer-rules</artifactId>
+          <version>0.0.1-SNAPSHOT</version>
+        </dependency>
+      </dependencies>
+
+    </plugin>
+    ...
+  </plugins>
+</build>
+
+```
---
+++ bitcoinj-enforcer-rules/src/main/java/com/google/bitcoinj/enforcer/DigestRule.java
@@ -0,0 +1,176 @@
+package com.google.bitcoinj.enforcer;
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.factory.ArtifactFactory;
+import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.artifact.resolver.ArtifactNotFoundException;
+import org.apache.maven.artifact.resolver.ArtifactResolutionException;
+import org.apache.maven.artifact.resolver.ArtifactResolver;
+import org.apache.maven.artifact.versioning.VersionRange;
+import org.apache.maven.enforcer.rule.api.EnforcerRule;
+import org.apache.maven.enforcer.rule.api.EnforcerRuleException;
+import org.apache.maven.enforcer.rule.api.EnforcerRuleHelper;
+import org.apache.maven.plugin.logging.Log;
+import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.component.configurator.expression.ExpressionEvaluationException;
+import org.codehaus.plexus.component.repository.exception.ComponentLookupException;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Formatter;
+
+/**
+ * <p>Enforcer rule to provide the following to build process:</p>
+ * <ul>
+ * <li>Checking of hashes against an expected list</li>
+ * </ul>
+ *
+ * @since 0.0.1
+ *         
+ */
+public class DigestRule implements EnforcerRule {
+
+  private String[] urns = null;
+
+
+  public String getCacheId() {
+    return "id"; // This is not cacheable
+  }
+
+  public boolean isCacheable() {
+    return false;
+  }
+
+  public boolean isResultValid(EnforcerRule arg0) {
+    return false;
+  }
+
+  public void execute(EnforcerRuleHelper helper) throws EnforcerRuleException {
+
+    Log log = helper.getLog();
+
+    boolean failed=false;
+
+    try {
+      // get the various expressions out of the helper.
+      MavenProject project = (MavenProject) helper.evaluate("${project}");
+
+      ArtifactRepository localRepository;
+      ArtifactFactory artifactFactory;
+      ArtifactResolver resolver;
+      try {
+        localRepository = (ArtifactRepository) helper.evaluate("${localRepository}");
+
+        artifactFactory = (ArtifactFactory) helper.getComponent(ArtifactFactory.class);
+
+        resolver = (ArtifactResolver) helper.getComponent(ArtifactResolver.class);
+
+      } catch (ComponentLookupException e) {
+        throw new EnforcerRuleException("Failing because a component lookup failed", e);
+      }
+
+      if (urns == null) {
+        throw new EnforcerRuleException("Failing because there are no URNs in the <configuration> section. See the README for help.");
+      }
+
+      for (String urn : urns) {
+        log.info("Verifying URN: " + urn);
+        // Decode it into artifact co-ordinates
+        String[] coordinates = urn.split(":");
+        if (coordinates.length != 8) {
+          throw new EnforcerRuleException("Failing because URH '" + urn + "' is not in format 'groupId:artifactId:version:type:classifier:scope:algorithm:hash'");
+        }
+
+        String groupId = coordinates[0];
+        String artifactId = coordinates[1];
+        String version = coordinates[2];
+        String type = coordinates[3];
+        String classifier = "null".equalsIgnoreCase(coordinates[4]) ? null : coordinates[4];
+        String scope = coordinates[5];
+        String algorithm = coordinates[6];
+        String hash = coordinates[7];
+
+        VersionRange versionRange = VersionRange.createFromVersion(version);
+        Artifact artifact = artifactFactory.createDependencyArtifact(groupId, artifactId, versionRange, type, classifier, scope);
+        try {
+          resolver.resolve(artifact, project.getRemoteArtifactRepositories(), localRepository);
+        } catch (ArtifactResolutionException e) {
+          throw new EnforcerRuleException("Failing due to artifact resolution ", e);
+        } catch (ArtifactNotFoundException e) {
+          throw new EnforcerRuleException("Failing due to artifact not found", e);
+        }
+
+        String actual = digest(artifact.getFile(), algorithm);
+        if (!actual.equals(hash)) {
+          log.error("*** CRITICAL FAILURE *** Artifact does not match. Possible side-chain attack. Expected='" + hash + "' Actual='" + actual + "'");
+          failed = true;
+        }
+      }
+
+      if (failed) {
+        throw new EnforcerRuleException("At least one artifact has not met expectations.");
+      }
+
+    } catch (ExpressionEvaluationException e) {
+      throw new EnforcerRuleException("Unable to lookup an expression " + e.getLocalizedMessage(), e);
+    }
+  }
+
+  /**
+   * @param file The file to digest
+   * @param algo The algorithm to use (e.g. "sha1", "md5")
+   *
+   * @return The hex version of the digest
+   *
+   * @throws EnforcerRuleException If something goes wrong
+   */
+  private String digest(File file, String algo) throws EnforcerRuleException {
+    try {
+      // Create a fresh digest every time
+      MessageDigest md = MessageDigest.getInstance(algo);
+
+      // Wrap a digest around the file input stream
+      FileInputStream fis = new FileInputStream(file);
+      DigestInputStream dis = new DigestInputStream(fis, md);
+
+      // Read fully to get digest
+      while (dis.read() != -1) {
+      }
+
+      // Clean up resources
+      dis.close();
+      fis.close();
+      byte[] digest = md.digest();
+
+      return byteToHex(digest);
+
+    } catch (NoSuchAlgorithmException e) {
+      throw new EnforcerRuleException("Unable to digest " + e.getLocalizedMessage(), e);
+    } catch (FileNotFoundException e) {
+      throw new EnforcerRuleException("Unable to digest " + e.getLocalizedMessage(), e);
+    } catch (IOException e) {
+      throw new EnforcerRuleException("Unable to digest " + e.getLocalizedMessage(), e);
+    }
+  }
+
+  /**
+   * @param digest The digest of the message
+   *
+   * @return The hex version
+   */
+  private static String byteToHex(final byte[] digest) {
+    Formatter formatter = new Formatter();
+    for (byte b : digest) {
+      formatter.format("%02x", b);
+    }
+    String result = formatter.toString();
+    formatter.close();
+    return result;
+  }
+
+}
